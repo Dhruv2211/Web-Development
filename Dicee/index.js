0fb9ea7e4fb1d2e7cdec3699ad3fa2a8feb13263
Dhruv2211 <43109992+Dhruv2211@users.noreply.github.com>
2023-06-16 23:09:13 +0530

    Add files via upload

---
+++ Dicee/index.js
@@ -0,0 +1,24 @@
+var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+
+var dice1 = "dice"+randomNumber1+".png";
+var dice1Source = "./images/"+dice1;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", dice1Source);
+
+var dice2 = "dice"+randomNumber2+".png";
+var dice2Source = "./images/"+dice2;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", dice2Source);
+
+
+
+var player1 = "Player 1 wins";
+var player2 = "Player 2 wins";
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = player1;
+}
+else{
+    document.querySelector("h1").innerText = player2;
+}
